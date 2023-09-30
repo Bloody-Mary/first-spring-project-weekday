b465ca795b47a1953b511c2e594b7fbd9098f074
Bloody-Mary <marianelalieben@gmail.com>
2023-09-30 15:09:53 +0300

    Initial commit

---
+++ src/main/java/ru/itgirl/weekdayspringapplication/WeekdaySpringApplication.java
@@ -0,0 +1,14 @@
+package ru.itgirl.weekdayspringapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeekdaySpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeekdaySpringApplication.class, args);
+	}
+
+}
+ 
---
+++ src/test/java/ru/itgirl/weekdayspringapplication/FirstSpringProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.itgirl.weekdayspringapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstSpringProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
